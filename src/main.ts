26dd7ca89e9575fd5af5b5e859fd9b72588cf542
Julián Tobito <julianmontoua@gmail.com>
2026-04-25 14:05:17 -0500

    feat(New Socket.io): :rocket: start WebSocket connection

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  let port: number = Number(process.env.PORT);
+
+  await app.listen(port, () => {
+    console.log('Servidor escuchando el puerdo:', port);
+  });
 }
 bootstrap();
